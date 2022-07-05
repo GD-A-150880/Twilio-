eb804913c8cafa782a1408961c0d84a37fc11304
Muhammad Hisham Akram <36224246+GD-A-150880@users.noreply.github.com>
2022-07-05 05:00:20 -0700

    commit

---
+++ sms/sendbulksms.php
@@ -0,0 +1,38 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+// Use the REST API Client to make requests to the Twilio REST API
+use Twilio\Rest\Client;
+echo "1";
+// Your Account AccountSid and Auth Token from twilio.com/console
+$AccountSid = 'AC0d7b410f970e88e8cdf7eb7bc58c457b';
+$AuthToken = '14fba74ec94646d1b04ce2c3a2015656';
+$client = new Client($AccountSid, $AuthToken);
+echo "1";
+
+// Use the client to do fun stuff like send text messages!
+
+// An array of users(receivers) containing Mobile Number & Name.
+// +1 is code of country(US). And xxxxxxxxxx is user's mobile number.
+$people = array(
+	'715-201-6035' => "Chirag",
+	'715-446-7120' => "Matt",
+
+);
+echo "1";
+
+foreach ($people as $number => $name) {
+	$sms = $client->account->messages->create(
+		$number,
+		array(
+			'from' => "8022789488", //phone number from twilio.com
+			'body' => "Bulk sms test "
+		)
+	); 
+//	echo "Sent message to $name <br><br>";
+}
+
+echo "1";
+
+
+?>
